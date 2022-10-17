48fe9415ec6f3a729f4fef6a1f0b7a6fd7374283
Ciahaka <wilddiver.by@gmail.com>
2022-10-17 23:05:51 +0300

    add Skills

---
+++ src/Component/skills/Skills.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import classes from './Skills.module.css';
+import classesContainer from '../../common/classes/Container.module.css'
+
+export const Skills = () => {
+  return (
+    <div className={classes.skillsBlock}>
+<div className={classesContainer.container}></div>
+    </div>
+  );
+}
